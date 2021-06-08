0373b76e2cabf0741b366ae034c04c9da7b9434a
E-Bousk <85066311+E-Bousk@users.noreply.github.com>
2021-06-08 23:01:14 +0200

    V2.0
    resize font, more media queries, toggle menu, toggle button

--- assets/js/commun.js
+++ assets/js/commun.js
@@ -15,6 +15,7 @@
 // =====================================================================
 
 
+// to toggle on/off the items on menu
 function openCloseMenu(divID, imgID) {
 
 	if ($(divID).is(":visible")) {
@@ -33,5 +34,38 @@ function openCloseMenu(divID, imgID) {
 		});
 	}
 }
+
+
+// to toggle on/off the entire menu
+function toggleMenu() {
+
+	if ($("#navSize").is(":visible")) {
+		$("#navSize").toggle("slow");
+		$("#navSize").attr("class", "col-2");
+	} else {
+		$("#navSize").toggle("slow");
+		$("#navSize").attr("class", "d-none d-md-block col-md-2");
+	}
+}
+
+
+// to toggle with the 3 dots
+function toggleButton() {
+	
+	$("#executeCode").toggle("slow");
+}
+
+
+// function myFunction(x) {
+// 	if (x.matches) { // If media query matches
+// 	  document.body.style.backgroundColor = "yellow";
+// 	} else {
+// 	 document.body.style.backgroundColor = "pink";
+// 	}
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
 
 
--- assets/php/BOUSKSOU_EDDY.php
+++ assets/php/BOUSKSOU_EDDY.php
@@ -10,95 +10,85 @@
     * @copyright 2021-2022 e.bousk@gmail.com
     * @version v1.0
     */
-    class CV
-    {
-    private $_sTitreFront= "Développer la partie front-end d'une application web ou web mobile en intégrant
-    les recommandations de sécurité ";
-    private $_sDetailFront= "
-        - Maquette et réalisation graphique de site internet <br>
-        - Réaliser une interface utilisateur web statique et adaptable <br>
-        - Développer une interface utilisateur web dynamique <br>
-        - Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce <br> 
-    ";
+    class CV {
+        private $_sTitreFront= "
+            Développer la partie front-end d'une application web ou web mobile en intégrant les recommandations de sécurité ";
+        private $_sDetailFront= "
+            - Maquette et réalisation graphique de site internet <br>
+            - Réaliser une interface utilisateur web statique et adaptable <br>
+            - Développer une interface utilisateur web dynamique <br>
+            - Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce <br>";
 
-    private $_sTitreBack= "Développer la partie back-end d'une application web ou web mobile en intégrant
-    les recommandations de sécurité ";
-    private $_sDetailBack= "
-        - Créer une base de donnée <br>
-        - Développer les composants d'accès aux données <br>
-        - Développer la partie back-end d'une application web ou web mobile <br>
-        - Elaborer et mettre en œuvre des composants dans une application de gestion de contenu ou e-commerce <br>
-    ";
+        private $_sTitreBack= "
+            Développer la partie back-end d'une application web ou web mobile en intégrant les recommandations de sécurité ";
+        private $_sDetailBack= "
+            - Créer une base de donnée <br>
+            - Développer les composants d'accès aux données <br>
+            - Développer la partie back-end d'une application web ou web mobile <br>
+            - Elaborer et mettre en œuvre des composants dans une application de gestion de contenu ou e-commerce <br>";
 
-    protected function _afficher_competences()
-    {
-        echo $this->_sTitreFront . " :<br>" . $this->_sDetailFront . "<br>";
-        echo $this->_sTitreBack . " :<br>" . $this->_sDetailBack . "<br>";
-    }
-
-    protected function _afficher_experiences_professionnelles($sExperiencesProfesionnelles)
-    {
-        switch ($sExperiencesProfesionnelles) {
-            case "2014-2020":
-                echo "Responsable rayon fruits & légumes - BIOCOOP Lunel <br>";
-                break;
-            case "2009-2010":
-                echo "Animateur atelier informatique École - Marie Curie Lunel <br>";
-                break;
-            case "2000-2007":
-                echo "Technicien, monteur, réparateur, vendeur informatique - SARL CASH CONVERTERS Montpellier <br>";
-                break;
-            case "1992-1997":
-                echo "Agent de maîtrise (« leader future expansion ») - Eurodisney SCA Marne-la-vallée <br>";
-                break;
+        protected function _afficher_competences() {
+            echo "<b>" . $this->_sTitreFront . " :</b><br>" . $this->_sDetailFront . "<br>";
+            echo "<b>" . $this->_sTitreBack . " :</b><br>" . $this->_sDetailBack . "<br>";
         }
-    }
 
-    protected function _afficher_infos_complementaires()
-    {
-        echo "Anglais, Autonomie, Capacité d'adaptation, Réactivité, Sens de l'organisation, Travail en équipe. ";
-    }
+        protected function _afficher_experiences_professionnelles($sExperiencesProfesionnelles) {
+            switch ($sExperiencesProfesionnelles) {
+                case "2014-2020":
+                    echo "Responsable rayon fruits & légumes - BIOCOOP Lunel (" . $sExperiencesProfesionnelles . ") <br>";
+                    break;
+                case "2009-2010":
+                    echo "Animateur atelier informatique École - Marie Curie Lunel (" . $sExperiencesProfesionnelles . ") <br>";
+                    break;
+                case "2000-2007":
+                    echo "Technicien, monteur, réparateur, vendeur informatique - SARL CASH CONVERTERS Montpellier (" . $sExperiencesProfesionnelles . ") <br>";
+                    break;
+                case "1992-1997":
+                    echo "Agent de maîtrise (« leader future expansion ») - Eurodisney SCA Marne-la-vallée (" . $sExperiencesProfesionnelles . ") <br>";
+                    break;
+            }
+        }
+
+        protected function _afficher_infos_complementaires() {
+            echo "Anglais, Autonomie, Capacité d'adaptation, Réactivité, Sens de l'organisation, Travail en équipe. ";
+        }
     }
 
     /**
     * Class Competences | fichier BOUSKSOU_EDDY.php
     */
-    class Competences extends CV
-    {
-    public function __construct()
-    {
-        parent::_afficher_competences();
-    }
+    class Competences extends CV {
+        public function __construct() {
+            parent::_afficher_competences();
+        }
     }
 
     /**
     * Class ExperiencesProfesionnelles | fichier BOUSKSOU_EDDY.php
     */
-    class ExperiencesProfesionnelles extends CV
-    {
-    public function __construct($sExperiencesProfesionnelles)
-    {
-        parent::_afficher_experiences_professionnelles($sExperiencesProfesionnelles);
-    }
+    class ExperiencesProfesionnelles extends CV {
+        public function __construct($sExperiencesProfesionnelles) {
+            parent::_afficher_experiences_professionnelles($sExperiencesProfesionnelles);
+        }
     }
 
     /**
     * Class OtherAssets | fichier BOUSKSOU_EDDY.php
     */
-    class OtherAssets extends CV
-    {
-    public function __construct()
-    {
-        parent::_afficher_infos_complementaires();
-    }
+    class OtherAssets extends CV {
+        public function __construct() {
+            parent::_afficher_infos_complementaires();
+        }
     }
 
     $oCompetences= new Competences();
-
+    
+    echo "<b> Experiences Profesionnelles : </b></br>";
     $oExperiences1= new ExperiencesProfesionnelles("2014-2020");
     $oExperiences2= new ExperiencesProfesionnelles("2009-2010");
     $oExperiences3= new ExperiencesProfesionnelles("2000-2007");
     $oExperiences4= new ExperiencesProfesionnelles("1992-1997");
 
+    echo "</br><b> Divers : </b></br>";
     $oAssets= new OtherAssets();
-    ?>
+?>
